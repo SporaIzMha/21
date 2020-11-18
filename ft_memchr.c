99104cd7868b8d9a0d23db4b5b85db3e17ace647
Владимир Страчков <vovasporiy@gmail.com>
2020-11-19 00:14:32 +0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,19 +14,14 @@
 
 void	*ft_memchr(const void *str, int c, size_t num)
 {
-	unsigned char		*s;
-	unsigned char		cc;
-	size_t				i;
-
-	s = (unsigned char *)str;
-	cc = (unsigned char)c;
+	size_t	i;
+	
 	i = 0;
 	while (i < num)
 	{
-		if (*s == cc)
-			return (s);
+		if (*((unsigned char *)str + i) == (unsigned char)c)
+			return ((void *)str + i;
 		i++;
-		s++;
 	}
 	return (NULL);
 }
