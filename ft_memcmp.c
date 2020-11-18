360b3238d30028379339c6b87a7b90715895200a
Владимир Страчков <vovasporiy@gmail.com>
2020-11-19 00:18:33 +0300

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,13 +14,15 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	size_t i;
+	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (i < n)
 	{
-		if ((*(unsigned char*)(str1 + i)) != (*(unsigned char*)(str2 + i)))
-			return (*(unsigned char*)(str1 + i) - *(unsigned char*)(str2 + i));
+		if (*((unsigned char*)str1 + i) != *((unsigned char*)str2 + i))
+			return (*((unsigned char*)str1 + i) - *((unsigned char*)str2 + i));
 		i++;
 	}
 	return (0);
