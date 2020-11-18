cadebd04c1b2e20f09a5d47a76b4bb39bcf41f1d
Владимир Страчков <vovasporiy@gmail.com>
2020-11-19 00:10:52 +0300

    Update ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -16,11 +16,13 @@ void	*ft_memccpy(void *str, const void *src, int c, size_t num)
 {
 	size_t i;
 
+	if (num < 1)
+		return (NULL);
 	i = 0;
 	while (i < num)
 	{
-		*(unsigned char*)(str + i) = *(unsigned char*)(src + i);
-		if (*(unsigned char*)(src + i) == (unsigned char)c)
+		*((unsigned char *)str + i) = *((unsigned char*)src + i);
+		if (*((unsigned char *)src + i) == (unsigned char)c)
 			return (str + 1 + i);
 		i++;
 	}
