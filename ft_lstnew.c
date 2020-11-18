549d2cc3204bb1815223e7dd8b6ed0c3a54f8001
Владимир Страчков <vovasporiy@gmail.com>
2020-11-18 23:23:56 +0300

    Update ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list *e;
 
-	if (!(e = (t_list*)malloc(sizeof(*e))))
+	if (!(e = (t_list*)malloc(sizeof(e))))
 		return (NULL);
 	e->content = content;
 	e->next = NULL;
